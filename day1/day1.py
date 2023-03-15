f3bed5a1861d5c77db5f0dddb71bdde91e31410a
ericpham <phamwangthang@gmail.com>
2023-03-16 00:50:53 +0100

    part 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,17 @@
 with open('day1/day1.txt') as file:
-	content = file.readlines()
+	content = file.read().strip().split("\n")
 
-print(content)
+# print(content)
+
+calories = 0
+max = 0
+
+for item in content:
+	if item == '':
+		calories = 0
+	else:
+		calories += int(item)
+		if calories > max:
+			max = calories
+
+print(max)
