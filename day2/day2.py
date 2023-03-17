305616069ab03f2e0500dccafb3127097873208c
ericpham <phamwangthang@gmail.com>
2023-03-17 23:31:12 +0100

    Part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,9 +1,6 @@
 with open('day2/day2.txt') as file:
 	content = file.read().strip().split('\n')
 
-# print(content)
-
-# Part 1
 combo = {
 'A X' : 4,
 'A Y': 8,
@@ -22,3 +19,22 @@ for round in content:
 	total = total + combo[round]
 
 print(total)
+
+combo_2 = {
+'A X' : 0 + 3,
+'A Y': 3 + 1,
+'A Z': 6 + 2,
+'B X' : 0 + 1,
+'B Y' : 3 + 2,
+'B Z' : 6 + 3,
+'C X' : 0 + 2,
+'C Y' : 3 + 3,
+'C Z' : 6 + 1
+}
+
+total2 = 0
+
+for round in content:
+	total2 = total2 + combo_2[round]
+
+print(total2)
