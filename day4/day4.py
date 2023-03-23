f787a5576eef3ae5bf5f342c73ab0e80a09f3512
ericpham <phamwangthang@gmail.com>
2023-03-23 21:48:46 +0100

    Part 1

--- day4/day4.py
+++ day4/day4.py
@@ -1,13 +1,12 @@
 with open('day4/day4.txt') as file:
 	content = file.readlines()
 
-# print(content)
+# Part 1
 total = 0
 for pairs in content:
-	section = pairs.split(",")
-
-	first_elf = (section[0].split("-"))
-	second_elf = section[1].split("-")
+	section = pairs.split(",") #['38-41','38-38']
+	first_elf = (section[0].split("-")) #[38-41]
+	second_elf = section[1].split("-") #[38-38]
 	if (int(first_elf[0]) >= int(second_elf[0]) and int(first_elf[1]) <= int(second_elf[1])) or (int(first_elf[0]) <= int(second_elf[0]) and int(first_elf[1]) >= int(second_elf[1])):
 		total += 1
 
