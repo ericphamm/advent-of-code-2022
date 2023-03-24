3f1aea4708ece32de649c481ceabb3395037c998
ericpham <phamwangthang@gmail.com>
2023-03-24 21:59:03 +0100

    Part 2

--- day4/day4.py
+++ day4/day4.py
@@ -11,3 +11,14 @@ for pairs in content:
 		total += 1
 
 print(total)
+
+# Part 2
+overlapped = 0
+for pairs in content:
+	section = pairs.split(",")
+	first_elf = section[0].split("-")
+	second_elf = section[1].split("-")
+	if(int(second_elf[0]) >= int(first_elf[0]) and int(second_elf[0]) <= int(first_elf[1])) or (int(second_elf[1]) >= int(first_elf[0]) and int(second_elf[1]) <= int(first_elf[1])) or (int(first_elf[0]) >= int(second_elf[0]) and int(first_elf[0]) <= int(second_elf[1])) or (int(first_elf[1]) >= int(second_elf[0]) and int(first_elf[1]) <= int(second_elf[1])):
+		overlapped += 1
+
+print(overlapped)
