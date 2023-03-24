0f29b85332404d7d1d0fddb6e1227411cbd1cb89
ericpham <phamwangthang@gmail.com>
2023-03-24 22:01:15 +0100

    Part 2

--- day4/day4.py
+++ day4/day4.py
@@ -18,7 +18,8 @@ for pairs in content:
 	section = pairs.split(",")
 	first_elf = section[0].split("-")
 	second_elf = section[1].split("-")
-	if(int(second_elf[0]) >= int(first_elf[0]) and int(second_elf[0]) <= int(first_elf[1])) or (int(second_elf[1]) >= int(first_elf[0]) and int(second_elf[1]) <= int(first_elf[1])) or (int(first_elf[0]) >= int(second_elf[0]) and int(first_elf[0]) <= int(second_elf[1])) or (int(first_elf[1]) >= int(second_elf[0]) and int(first_elf[1]) <= int(second_elf[1])):
+	if(int(second_elf[0]) >= int(first_elf[0]) and int(second_elf[0]) <= int(first_elf[1])) or (int(second_elf[1]) >= int(first_elf[0]) and int(second_elf[1]) <= int(first_elf[1])) or \
+	(int(first_elf[0]) >= int(second_elf[0]) and int(first_elf[0]) <= int(second_elf[1])) or (int(first_elf[1]) >= int(second_elf[0]) and int(first_elf[1]) <= int(second_elf[1])):
 		overlapped += 1
 
 print(overlapped)
