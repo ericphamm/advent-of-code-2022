f69d1019c8cb7163c5662cf4637dd2991c291320
ericpham <phamwangthang@gmail.com>
2023-04-08 23:34:15 +0200

    displayStack

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,13 @@
 with open('day5/day5.txt') as file:
-	stacks, instructions = (i.splitlines() for i in file.read().split("\n\n"))
+	stacks_strings, instructions = (i.splitlines() for i in file.read().split("\n\n"))
 
-print(stacks, instructions)
+stacks = {int(digit):[] for digit in stacks_strings[-1].replace(" ","")}
+# print(stacks, instructions)
 
+def displayStacks():
+	print("\n\nStacks:\n")
+	for stack in stacks:
+		print(stack, stacks[stack])
+	print("\n")
+
+displayStacks()
