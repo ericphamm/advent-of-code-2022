86a407640696c6cd1401f1350f61af5e9995aa73
ericpham <phamwangthang@gmail.com>
2023-04-14 23:42:41 +0200

    loadStack

--- day5/day5.py
+++ day5/day5.py
@@ -3,6 +3,7 @@ with open('day5/day5.txt') as file:
 
 stacks = {int(digit):[] for digit in stacks_strings[-1].replace(" ","")}
 # print(stacks, instructions)
+indexes = [index for index, char in enumerate(stacks_strings[-1]) if char != " "]
 
 def displayStacks():
 	print("\n\nStacks:\n")
@@ -10,4 +11,13 @@ def displayStacks():
 		print(stack, stacks[stack])
 	print("\n")
 
+def loadStack():
+	for string in stacks_strings[:-1]:
+		stack_num = 1
+		for index in indexes:
+			if string[index] != " ":
+				stacks[stack_num].insert(0, string[index])
+			stack_num += 1
+
+loadStack()
 displayStacks()
